1ad5e90f98b724d2d8ffb8f9642f98dd4d87f541
Thomas <tho.daskalakis@gmail.com>
2022-08-26 16:11:28 +0300

    Remove documentation

--- script.js
+++ script.js
@@ -1,234 +1,214 @@
 // Get DOM elements
 
-const formOverlay = document.querySelector('.add-book-overlay');
-formOverlay.style.display = 'none';
+const formOverlay = document.querySelector(".add-book-overlay");
+formOverlay.style.display = "none";
 
-const form = formOverlay.querySelector('form');
+const form = formOverlay.querySelector("form");
 
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 
-const mainGrid = container.querySelector('.main-grid');
+const mainGrid = container.querySelector(".main-grid");
 
-const addBookButton = mainGrid.querySelector('button.add-book');
+const addBookButton = mainGrid.querySelector("button.add-book");
 
-addBookButton.textContent = 'Add New Book';
+addBookButton.textContent = "Add New Book";
 
-addBookButton.addEventListener('click', (e) => {
-    setFormDisplay('flex');
+addBookButton.addEventListener("click", (e) => {
+  setFormDisplay("flex");
 });
 
 // Get form elements
 
-const titleField = formOverlay.querySelector('#title');
-const authorField = formOverlay.querySelector('#author');
-const pagesField = formOverlay.querySelector('#pages');
-const readField = formOverlay.querySelector('#read');
+const titleField = formOverlay.querySelector("#title");
+const authorField = formOverlay.querySelector("#author");
+const pagesField = formOverlay.querySelector("#pages");
+const readField = formOverlay.querySelector("#read");
 
-const confirmFormBtn = document.querySelector('.confirm-form-btn');
+const confirmFormBtn = document.querySelector(".confirm-form-btn");
 
-confirmFormBtn.addEventListener('click', (e) => {
-    addBookToLibrary(
+confirmFormBtn.addEventListener("click", (e) => {
+  addBookToLibrary(
     titleField.value,
     authorField.value,
     pagesField.value,
-    readField.checked,
-    );
+    readField.checked
+  );
 
-    setFormDisplay('none');
-    form.reset();
+  setFormDisplay("none");
+  form.reset();
 });
 
-const cancelFormBtn = document.querySelector('.cancel-form-btn');
+const cancelFormBtn = document.querySelector(".cancel-form-btn");
 
-cancelFormBtn.addEventListener('click', (e) => {
-    setFormDisplay('none');
-    form.reset();
+cancelFormBtn.addEventListener("click", (e) => {
+  setFormDisplay("none");
+  form.reset();
 });
 
-/**
- * Current library of books.
- */
 let myLibrary = [];
 
-/**
- * Book constructor.
- * @param {String} name Book's name
- * @param {String} author Book's author
- * @param {Number} pages Number of pages
- * @param {Boolean} read Is read
- * @param {Number} index Index in library
- */
 function Book(name, author, pages, read, index) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.index = index;
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.index = index;
 }
 
-/**
- * Returns information about the book.
- * @returns {String} Book information
- */
-Book.prototype.info = function() {
-    const isRead = this.read ? 'is read' : 'not read yet';
-    return `${this.name} by ${this.author}, ${this.pages} pages, ${isRead}.`
+Book.prototype.info = function () {
+  const isRead = this.read ? "is read" : "not read yet";
+  return `${this.name} by ${this.author}, ${this.pages} pages, ${isRead}.`;
+};
+
+function addBookToLibrary(name, author, pages, read, index = myLibrary.length) {
+  myLibrary.push(new Book(name, author, pages, read, index));
+
+  updateLibraryDisplay();
 }
 
-/**
- * Adds a new Book to the library.
- * @see {@link myLibrary}
- * @param {String} name Book's name
- * @param {String} author Book's author
- * @param {Number} pages Number of pages
- * @param {Boolean} read Is read
- */
-function addBookToLibrary(name, author, pages, read, 
-    index = myLibrary.length) {
-    myLibrary.push(new Book(name, author, pages, read, index));
-
-    updateLibraryDisplay();
-}
-
-/**
- * 
- * @param {Book} book The book to be put in the card
- * @returns A card element containing book information
- */
 function createCard(book) {
-    const card = document.createElement('div');
-    card.classList.add('card');
+  const card = document.createElement("div");
+  card.classList.add("card");
 
-    const sideColor = document.createElement('div');
-    sideColor.classList.add('side-color');
-    card.appendChild(sideColor);
+  const sideColor = document.createElement("div");
+  sideColor.classList.add("side-color");
+  card.appendChild(sideColor);
 
-    const cardTitle = document.createElement('div');
-    cardTitle.classList.add('title');
-    cardTitle.textContent = book.name;
-    card.appendChild(cardTitle);
+  const cardTitle = document.createElement("div");
+  cardTitle.classList.add("title");
+  cardTitle.textContent = book.name;
+  card.appendChild(cardTitle);
 
-    const content = document.createElement('div');
-    content.classList.add('content');
-    content.textContent = book.info();
-    card.appendChild(content);
+  const content = document.createElement("div");
+  content.classList.add("content");
+  content.textContent = book.info();
+  card.appendChild(content);
 
-    const actions = document.createElement('div');
-    actions.classList.add('actions');
-    
-    // Card actions
-    //
-    
-    // Delete
+  const actions = document.createElement("div");
+  actions.classList.add("actions");
 
-    const actionDelDiv = document.createElement('div');
-    actionDelDiv.classList.add('card-action-delete');
-    
-    const delBtn = document.createElement('button');
-    delBtn.addEventListener('click', (e) => {
-        deleteCard(book.index);
-    });
-    
-    const delImg = document.createElement('img');
-    delImg.setAttribute('src', './svg/delete_FILL0_wght400_GRAD0_opsz48.svg');
-    delBtn.appendChild(delImg);
-    
-    actionDelDiv.appendChild(delBtn);
-    actions.appendChild(actionDelDiv);
+  // Card actions
+  //
 
-    // Toggle read
+  // Delete
 
-    const actionReadDiv = document.createElement('div');
-    actionReadDiv.classList.add('card-action-read');
-    
-    const readBtn = document.createElement('button');
-    readBtn.addEventListener('click', (e) => {
-        toggleReadStatus(book, readBtn);
-    });
-    
-    const readImg = document.createElement('img');
-    readImg.setAttribute('src', 
-    './svg/visibility_FILL0_wght400_GRAD0_opsz48.svg');
+  const actionDelDiv = document.createElement("div");
+  actionDelDiv.classList.add("card-action-delete");
 
-    const notReadImg = document.createElement('img');
-    notReadImg.setAttribute('src', 
-    './svg/visibility_off_FILL0_wght400_GRAD0_opsz48.svg');
+  const delBtn = document.createElement("button");
+  delBtn.addEventListener("click", (e) => {
+    deleteCard(book.index);
+  });
 
-    if (book.read) {
-        readBtn.appendChild(readImg);
-    }
-    else {
-        readBtn.appendChild(notReadImg);
-    }
+  const delImg = document.createElement("img");
+  delImg.setAttribute("src", "./svg/delete_FILL0_wght400_GRAD0_opsz48.svg");
+  delBtn.appendChild(delImg);
 
-    actionReadDiv.appendChild(readBtn);
-    actions.appendChild(actionReadDiv);
+  actionDelDiv.appendChild(delBtn);
+  actions.appendChild(actionDelDiv);
 
-    card.appendChild(actions);
+  // Toggle read
 
-    return card;
+  const actionReadDiv = document.createElement("div");
+  actionReadDiv.classList.add("card-action-read");
+
+  const readBtn = document.createElement("button");
+  readBtn.addEventListener("click", (e) => {
+    toggleReadStatus(book, readBtn);
+  });
+
+  const readImg = document.createElement("img");
+  readImg.setAttribute(
+    "src",
+    "./svg/visibility_FILL0_wght400_GRAD0_opsz48.svg"
+  );
+
+  const notReadImg = document.createElement("img");
+  notReadImg.setAttribute(
+    "src",
+    "./svg/visibility_off_FILL0_wght400_GRAD0_opsz48.svg"
+  );
+
+  if (book.read) {
+    readBtn.appendChild(readImg);
+  } else {
+    readBtn.appendChild(notReadImg);
+  }
+
+  actionReadDiv.appendChild(readBtn);
+  actions.appendChild(actionReadDiv);
+
+  card.appendChild(actions);
+
+  return card;
 }
 
-/**
- * Updates app interface with current books. 
- * Should be called after every change in the library.
- * @see {@link myLibrary}
- */
 function updateLibraryDisplay() {
-    // Empty grid
-    const gridBooks = [...mainGrid.children];
-    gridBooks.slice(1, gridBooks.length).forEach(child => {
-        mainGrid.removeChild(child);
-    });
+  // Empty grid
+  const gridBooks = [...mainGrid.children];
+  gridBooks.slice(1, gridBooks.length).forEach((child) => {
+    mainGrid.removeChild(child);
+  });
 
-    // Add children from library
-    myLibrary.forEach(book => {
-        mainGrid.appendChild(createCard(book));
-    });
+  // Add children from library
+  myLibrary.forEach((book) => {
+    mainGrid.appendChild(createCard(book));
+  });
 }
 
 function setFormDisplay(d) {
-    formOverlay.style.display = d;
+  formOverlay.style.display = d;
 }
 
 function deleteCard(index) {
-    console.log(index);
-    myLibrary.splice(index, 1);
+  console.log(index);
+  myLibrary.splice(index, 1);
 
-    // Update indexes
+  // Update indexes
 
-    myLibrary.forEach(book => book.index = myLibrary.indexOf(book));
+  myLibrary.forEach((book) => (book.index = myLibrary.indexOf(book)));
 
-    updateLibraryDisplay();
+  updateLibraryDisplay();
 }
 
 function toggleReadStatus(book, readBtn) {
-    const readImg = document.createElement('img');
-    readImg.setAttribute('src', 
-    './svg/visibility_FILL0_wght400_GRAD0_opsz48.svg');
+  const readImg = document.createElement("img");
+  readImg.setAttribute(
+    "src",
+    "./svg/visibility_FILL0_wght400_GRAD0_opsz48.svg"
+  );
 
-    const notReadImg = document.createElement('img');
-    notReadImg.setAttribute('src', 
-    './svg/visibility_off_FILL0_wght400_GRAD0_opsz48.svg');
+  const notReadImg = document.createElement("img");
+  notReadImg.setAttribute(
+    "src",
+    "./svg/visibility_off_FILL0_wght400_GRAD0_opsz48.svg"
+  );
 
-    if (book.read) {
-        readBtn.childNodes.forEach(child => readBtn.removeChild(child));
-        readBtn.appendChild(notReadImg);
-    }
-    else {
-        readBtn.childNodes.forEach(child => readBtn.removeChild(child));
-        readBtn.appendChild(readImg);
-    }
+  if (book.read) {
+    readBtn.childNodes.forEach((child) => readBtn.removeChild(child));
+    readBtn.appendChild(notReadImg);
+  } else {
+    readBtn.childNodes.forEach((child) => readBtn.removeChild(child));
+    readBtn.appendChild(readImg);
+  }
 
-    book.read = !book.read;
+  book.read = !book.read;
 
-    updateLibraryDisplay();
+  updateLibraryDisplay();
 }
 
 // Add a default book to be displayed
 
-const defaultBook = new Book("Do Androids Dream of Electric Sheep?", 
-"Philip K. Dick", 210, true);
+const defaultBook = new Book(
+  "Do Androids Dream of Electric Sheep?",
+  "Philip K. Dick",
+  210,
+  true
+);
 
-addBookToLibrary(defaultBook.name, defaultBook.author, defaultBook.pages, 
-defaultBook.read);
+addBookToLibrary(
+  defaultBook.name,
+  defaultBook.author,
+  defaultBook.pages,
+  defaultBook.read
+);
